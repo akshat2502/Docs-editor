d9212437a658bed2c2da8fa38c01f50c11d01a26
Akshat Arora <akshatarora411@gmail.com>
2026-08-08 23:51:39 +0530

    feat: added new toolbar buttons

--- src/app/documents/[id]/editor.tsx
+++ src/app/documents/[id]/editor.tsx
@@ -4,15 +4,38 @@ import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { TaskItem, TaskList } from '@tiptap/extension-list';
 import Image from '@tiptap/extension-image';
+import Highlight from '@tiptap/extension-highlight';
 import { Dropcursor, Gapcursor } from '@tiptap/extensions'
 import { TableKit } from '@tiptap/extension-table'
+import { TextStyle, Color } from '@tiptap/extension-text-style'
+import { FontFamily } from '@tiptap/extension-font-family'
 import { useEditorStore } from "@/store/use-editor";
 
 const Editor = () => {
   const { setEditor } = useEditorStore();
   const editor = useEditor({
     onCreate: ({ editor }) => {
-      console.log("Editor created:", editor);
+      setEditor(editor);
+    },
+    onUpdate: ({ editor }) => {
+      setEditor(editor);
+    },
+    onDestroy: () => {
+      setEditor(null);
+    },
+    onSelectionUpdate: ({ editor }) => {
+      setEditor(editor);
+    },
+    onTransaction: ({ editor }) => {
+      setEditor(editor);
+    },
+    onFocus: ({ editor }) => {
+      setEditor(editor);
+    },
+    onBlur: ({ editor }) => {
+      setEditor(editor);
+    },
+    onContentError: ({ editor }) => {
       setEditor(editor);
     },
     extensions: [
@@ -25,6 +48,10 @@ const Editor = () => {
         TableKit.configure({
         table: { resizable: true },
       }),
+        TextStyle,
+        Color,
+        Highlight.configure({ multicolor: true }),
+        FontFamily,
     ],
     editorProps: {
       attributes: {
